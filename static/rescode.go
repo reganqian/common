20d99a4cec4dfc9dfb17e0a4fe84bea904174ada
reganqian <qianlaijian@gmail.com>
2023-02-20 09:23:41 +0800

    公共工具

--- static/rescode.go
+++ static/rescode.go
@@ -1,53 +1,65 @@
 package static
 
+type BaseReply struct {
+	ResCode string `json:"resCode"` //返回码
+	ResDesc string `json:"resDesc"` //返回描述
+}
 
+func (s *BaseReply) Success() {
+	s.ResCode = SUCCESS
+	s.ResDesc = DEFAULT_SUCCESS_DESC
+}
+
+func (s *BaseReply) Failed(resCode, resDesc string) {
+	s.ResCode = resCode
+	s.ResDesc = resDesc
+}
 
 const (
-	SUCCESS 	string 	= "success"	//成功
-	FAILED		string 	= "failed"	//失败
-	PARAMLOST	string	= "param_lost"	//参数缺失
-	PARAMERR	string	= "param_error"	//参数错误
-	SERVERERR	string	= "server_error"	//服务器异常
-	DBERROR		string	= "db_error"	//数据异常
-	
-	TOKENERROR	string   = "token_error"	//登录TOKEN错误
-	DATAEXIST	string 	= "data_exist"	//数据已存在
-	DATANOTEXIST string 	= "data_not_exist"	//数据不存在
-	NOTOWNER string = "not_owner"	//不是拥有者
-	FORMATERROR string = "format_error"	//格式错误
-	
-	TIMEOUT string = "time_out" //超时
-	NO_PERMISSION string = "no_permission" //没有权限
-	STATUSERROR string =  "status_error" //状态异常
-	DUPLICATE_DATA string = "duplicate_data" //重复数据
-	PWDERROR string = "pwd_error" //密码错误
-	NEED_LOGIN string = "need_login" //需要登录
+	SUCCESS   string = "SUCCESS"      //成功
+	FAILED    string = "FAILED"       //失败
+	PARAMLOST string = "PARAM_LOST"   //参数缺失
+	PARAMERR  string = "PARAM_ERROR"  //参数错误
+	SERVERERR string = "SERVER_ERROR" //服务器异常
+	DBERROR   string = "DB_ERROR"     //数据异常
+	STATUSERR string = "STATUS_ERR"
 
-	NEEDSHOW string = "need_show" //需要显示返回
+	TOKENERROR   string = "TOKEN_ERROR"    //登录TOKEN错误
+	DATAEXIST    string = "DATA_EXIST"     //数据已存在
+	DATANOTEXIST string = "DATA_NOT_EXIST" //数据不存在
+	NOTOWNER     string = "NOT_OWNER"      //不是拥有者
+	FORMATERROR  string = "FORMAT_ERROR"   //格式错误
 
-	MOREACTION string 	= "more_action"	//跟多操作
-	NEEDAUTH string = "need_auth"	//需要鉴权
-	AUTH_FAILED string = "auth_failed" //认证失败
+	TIMEOUT        string = "TIME_OUT"       //超时
+	NO_PERMISSION  string = "NO_PERMISSION"  //没有权限
+	STATUSERROR    string = "STATUS_ERROR"   //状态异常
+	DUPLICATE_DATA string = "DUPLICATE_DATA" //重复数据
+	PWDERROR       string = "PWD_ERROR"      //密码错误
+	NEED_LOGIN     string = "NEED_LOGIN"     //需要登录
 
-	NOT_ENOUGH string = "not_enough"//数据不足， 积分不足
-	
+	NEEDSHOW string = "NEED_SHOW" //需要显示返回
 
+	MOREACTION  string = "MORE_ACTION" //跟多操作
+	NEEDAUTH    string = "NEED_AUTH"   //需要鉴权
+	AUTH_FAILED string = "AUTH_FAILED" //认证失败
 
-	DEFAULT_SUCCESS_DESC string = "成功"
-	DATA_NOT_EXIST_DESC string = "数据不存在"
-	DATA_ALREADY_EXIST_DESC string = "数据已存在"
-	
-	NOTOWNER_DESC string = "不是拥有者"
-	FORMATERROR_DESC string = "格式错误"
-	
-	PARAMERR_DESC string = "参数异常"
-	DBERROR_DESC string = "数据库异常"
-	PARAMNULL_DESC string = "参数为空"
-	DEFAULT_FAILED_DESC string = "失败"
-	
-	NO_PERMISSION_DESC string = "没有权限"
-	NEED_LOGIN_DESC string = "需要登录"
-	STATUSERROR_DESC string = "状态异常"
-	DUPLICATE_DATA_DESC string = "重复数据"
-	PWDERROR_DESC string = "密码错误"
+	NOT_ENOUGH string = "NOT_ENOUGH" //数据不足， 积分不足
+
+	DEFAULT_SUCCESS_DESC    string = "success"
+	DATA_NOT_EXIST_DESC     string = "data is not exist"
+	DATA_ALREADY_EXIST_DESC string = "data is already exist"
+
+	NOTOWNER_DESC    string = "not the owner"
+	FORMATERROR_DESC string = "format error"
+
+	PARAMERR_DESC       string = "param error"
+	DBERROR_DESC        string = "data base error"
+	PARAMNULL_DESC      string = "param is null"
+	DEFAULT_FAILED_DESC string = "failed"
+
+	NO_PERMISSION_DESC  string = "permission deny"
+	NEED_LOGIN_DESC     string = "need login"
+	STATUSERROR_DESC    string = "status error"
+	DUPLICATE_DATA_DESC string = "duplicate data"
+	PWDERROR_DESC       string = "password is error"
 )
